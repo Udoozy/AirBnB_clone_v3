0db3bbc28ca50a45e7d093b1fd849bb7c3114e67
Udoozy <agbo4samson@gmail.com>
2024-08-05 13:29:04 +0100

    updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """
 This is to create variable app instance of Flask
 """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -11,8 +11,23 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+@app.teardown_appcontext
+def teardown_handler(exception):
+    """
+    This is handle error 404
+    """
+    storage.close()
 
-if __name__ == '__name__':
+@app.errorhanlder(404)
+def not_found(error):
+    """
+    This is coustomized error handler
+    """
+    response = {"error": "Not found"}
+    return jsonify(response), 404
+
+
+if __name__ == '__main__':
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = int(getenv('HBNB_API_PORT', '5000'))
-    app.run(host=HOST, port=PORT, threaded=True)
+    PORT = int(getenv('HBNB_API_PORT', 5000))
+    app.run(debug=True, host=HOST, port=PORT, threaded=True)
